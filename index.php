f7584a5f854b2a9dface9ac468b23411d9ba5644
mtallur567 <64742324+mtallur567@users.noreply.github.com>
2021-03-01 20:32:25 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<html>
+<header><title>This is title</title></header>
+<body>
+<?php print "I am running on host IP Address -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n"; ?>
+</body>
+</html>
